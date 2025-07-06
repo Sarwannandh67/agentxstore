03602b3e3cf8774c878794391391e4464e6c9e00
[firstname <creatornex@example.com>
2025-07-06 16:52:55 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vite"; 
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "./", // ✅ this fixes 404 on Vercel for JS/CSS assets
+
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [
-    react(),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
